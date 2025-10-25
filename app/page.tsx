fdebbaa413ba998d7bb44ffc73d9906f593522be
Gabriel Passos <miniruok109@gmail.com>
2025-10-25 00:00:06 -0300

    feat: 1° att

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,40 @@
-import Image from "next/image";
+import Link from "next/link";
+import "./globals.css";
+import styles from './MeuLink.module.css';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+    <div className={styles.container}>
+      <main>
+        <section className={styles.Section1}>
+          <div className={styles.headerWrapper}>
+            <div className={styles.brand}>
+              <h1>🥕 CenouraDesk</h1>
+            </div>
+
+            <nav className={styles.navLinks} aria-label="principal">
+              <Link href="#" className={styles.linkCustomizado}>Quero logar</Link>
+              <span className={styles.separator}>|</span>
+              <Link href="#" className={styles.linkCustomizado}>Criar conta</Link>
+            </nav>
+          </div>
+
+          <div className={styles.hero}>
+            <p className={styles.heroText}>
+              Plataforma que transforma seus agendamentos em crescimento, sem complicação.
+            </p>
+            <p className={styles.subText}>
+              A gestão de agendamentos e vendas definitiva para seu salão ou barbearia.
+            </p>
+
+            <div className={styles.cta}>
+              <button className={styles.ctaBtn}>Conhecer projeto</button>
+            </div>
+          </div>
+        </section>
+
+        <section id="sobre"></section>
       </main>
-    </div>
+    </div >
   );
 }
